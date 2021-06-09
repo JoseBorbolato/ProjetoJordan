4e6316064bd8e8a9d24748ca7e0797a167578462
José Luiz Borbolato <i17431@integrado.int>
2021-06-09 01:13:08 -0300

    Criando as tabelas com as diferênças entre os dias

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 import os
 import pandas as pd
-from pandas.core.frame import DataFrame
 import funcoes.conexao as conectModel
 import arq.consulta as cons
+from collections import defaultdict
+from datetime import datetime
 
 DIRARQ = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'arq')
 
@@ -26,10 +27,38 @@ except ValueError as erro:
 # Lendo uma consulta que esta dentro do pacote de "arq".consulta
 dataFramePulls = pd.read_sql_query(cons.consultauser_login(), con)
 
+
+dic = defaultdict(list)
 for indice, linha in dataFramePulls.iterrows():
-    print(
-        ' user_login->', linha['user_login'],
-        ' created_at->', linha['created_at'],
-        ' updated_at->', linha['updated_at'],
-        ' diferenca->', linha['diferenca']
-    )
+    dic[linha['user_login']] = {
+        'Data': [],
+        'Diferenca': []
+    }
+
+
+for indice, linha in dataFramePulls.iterrows():
+    dic[linha['user_login']]['Data'].append(linha['updated_at'])
+
+for chave, valor in dic.items():
+    size = len(valor['Data']) - 1
+    for x in range(size):
+        dic[chave]['Diferenca'].append(
+            abs(
+                (datetime.strptime(dic[chave]['Data'][x+1][:10], '%Y-%m-%d').date() -
+                 datetime.strptime(dic[chave]['Data'][x]
+                                   [:10], '%Y-%m-%d').date()
+                 ).days)
+        )
+    else:
+        dic[chave]['Diferenca'].append(0)
+
+
+for chave, valor in dic.items():
+    df = pd.DataFrame(dic[chave])
+    try:
+        df.to_sql(chave, con)
+    except ValueError as erro:
+        if str(erro) == f"Table '{chave}' already exists.":
+            print(f'A tabela "{chave}" já esta criada no banco"')
+        else:
+            print(f'Mensagem de erro: {erro}')
